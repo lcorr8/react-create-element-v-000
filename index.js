fbc7eb0544bd8cb9cdf223e6d4d425a26aa778e3
lcorr8 <l.corr.v@gmail.com>
2017-04-14 12:07:06 +0200

    rendering an element with children

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const title = React.createElement('h1', {}, 'My First React Code');
                                 //element or componene, properties, children of the element or component
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
 
+const container = React.createElement('div', {}, [title, paragraph]);
 ReactDOM.render(
   //thing you want to render
-  title,
+  container,
   //target dom node to render first argument into
   document.getElementById('main')
 );
