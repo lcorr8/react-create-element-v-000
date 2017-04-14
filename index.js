f8947b872954d5ac55d713370bb4f95d73c56dd3
lcorr8 <l.corr.v@gmail.com>
2017-04-14 12:16:08 +0200

    Done.

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ const list =
     React.createElement('h1', {}, 'My favorite ice cream flavors'),
     React.createElement('ul', {},
       [
-        React.createElement('li', {}, 'Chocolate'),
-        React.createElement('li', {}, 'Vanilla'),
-        React.createElement('li', {}, 'Banana')
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
       ]
     ));
  
-ReactDOM.render(list, document.getElementById('main'));
+ReactDOM.render(
+  list, 
+  document.getElementById('main')
+  );
