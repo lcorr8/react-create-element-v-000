8fc22b066924a62496a5f50d654a1be49bbe25f9
lcorr8 <l.corr.v@gmail.com>
2017-04-14 11:47:02 +0200

    working package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+const title = React.createElement('h1', {}, 'My First React Code');
+                                //element or componene, properties, children of the element or component
+
+ReactDOM.render(
+  //thing you want to render
+  title,
+  //target dom node to render first argument into
+  document.getElementById('main')
+);
